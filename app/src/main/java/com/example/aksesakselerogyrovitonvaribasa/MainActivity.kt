0f1eb738ed3be6de37b9e1019cb9b2a5b9803f83
vbsa909 <vitonvaribasa999@gmail.com>
2022-04-27 22:50:42 +0700

    akses akselero gyro viton varibasa

---
+++ app/src/main/java/com/example/aksesakselerogyrovitonvaribasa/MainActivity.kt
@@ -0,0 +1,75 @@
+package com.example.aksesakselerogyrovitonvaribasa
+
+import android.graphics.Color
+import android.hardware.Sensor
+import android.hardware.SensorEvent
+import android.hardware.SensorEventListener
+import android.hardware.SensorManager
+import android.os.Bundle
+import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.app.AppCompatDelegate
+
+class MainActivity : AppCompatActivity(), SensorEventListener {
+
+    private lateinit var sensorManager: SensorManager
+    private lateinit var square: TextView
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        // membuat handphone tetap dalam mode terang
+        AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO)
+
+        square = findViewById(R.id.tv_square)
+
+        setUpSensorStuff()
+    }
+
+    private fun setUpSensorStuff() {
+        // skrip ini membuat pengelola sensor
+        sensorManager = getSystemService(SENSOR_SERVICE) as SensorManager
+
+        // menentukan jenis sensor yang ingin digunakan
+        sensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER)?.also { accelerometer ->
+            sensorManager.registerListener(
+                this,
+                accelerometer,
+                SensorManager.SENSOR_DELAY_FASTEST,
+                SensorManager.SENSOR_DELAY_FASTEST
+            )
+        }
+    }
+
+    override fun onSensorChanged(event: SensorEvent?) {
+        // mengecek sensor yang sudah di pilih
+        if (event?.sensor?.type == Sensor.TYPE_ACCELEROMETER) {
+            val sides = event.values[0]
+            val upDown = event.values[1]
+
+            square.apply {
+                rotationX = upDown * 3f
+                rotationY = sides * 3f
+                rotation = -sides
+                translationX = sides * -10
+                translationY = upDown * 10
+            }
+
+            // mengubah warna persegi pada aplikasi apabila ponsel dalam keadaan rata
+            val color = if (upDown.toInt() == 0 && sides.toInt() == 0) Color.GREEN else Color.RED
+            square.setBackgroundColor(color)
+
+            square.text = "up/down ${upDown.toInt()}\nleft/right ${sides.toInt()}"
+        }
+    }
+
+    override fun onAccuracyChanged(sensor: Sensor?, accuracy: Int) {
+        return
+    }
+
+    override fun onDestroy() {
+        sensorManager.unregisterListener(this)
+        super.onDestroy()
+    }
+}
